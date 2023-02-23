0313ce159d348566b96bb14b799ac9e8b8957020
MrOctopus <shock.game@hotmail.com>
2023-02-23 02:57:05 +0100

    Migrated dev mode to using build tags

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 ## Requirements
 Roboto-Go needs the environment variable `CGO_ENABLED` set to `1` to build
+
+## Development
+Run Roboto-Go with the dev tag `go run -tags dev`
--- cmd/roboto/main.go
+++ cmd/roboto/main.go
@@ -4,51 +4,24 @@ import (
 	"flag"
 	"os"
 	"os/signal"
-	"path/filepath"
-	"runtime"
 
 	"github.com/Akvanvig/roboto-go/internal/bot"
-	"github.com/Akvanvig/roboto-go/internal/globals"
-	"github.com/rs/zerolog"
+	"github.com/Akvanvig/roboto-go/internal/util"
 	"github.com/rs/zerolog/log"
 )
 
 func main() {
 	// Arguments
-	var (
-		dev   = flag.Bool("dev", false, "Enable dev mode")
-		token = flag.String("token", "", "Bot access token")
-	)
+	var token = flag.String("token", "", "Bot access token")
 
 	// Setup
-	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
-	flag.Parse()
-
-	if *dev {
-		// Note(Fredrico).
-		// If we are running in dev mode, we automatically set the RootPath to be the same as go.mod's directory
-		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
-
-		log.Warn().Msg("Dev mode is enabled, do not use this flag in production")
-
-		_, mainPath, _, _ := runtime.Caller(0)
-		globals.RootPath = filepath.Join(filepath.Dir(mainPath), "../..")
-	} else {
-		// Note(Fredrico):
-		// Else, set RootPath to executable path
-		execPath, err := os.Executable()
-
-		if err != nil {
-			log.Fatal().Err(err).Msg("Failed to find running executable path")
-		}
-
-		globals.RootPath = filepath.Dir(execPath)
-	}
+	util.SetupRuntimeEnvironment()
 
 	if *token == "" {
 		log.Fatal().Msg("Token argument can not be empty")
 	}
 
+	// Run
 	channel := make(chan os.Signal, 1)
 	signal.Notify(channel, os.Interrupt)
 
